26b11a98706d6093e6a1796bfe27890dcc7d28a6
Bunyamin Aktas <bunyaminaktas@outlook.com>
2023-08-20 16:02:19 -0700

    - A guide for installing an encrypted system with an encrypted boot is prepared - A configuration guide for 4K scaling is prepared.

--- install_xfce.sh
+++ install_xfce.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter gvfs network-manager-applet pulseaudio pavucontrol mate-calc
+pacman -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter gvfs network-manager-applet pulseaudio pavucontrol mate-calc ntfs-3g xarchiver unrar unzip smplayer 
 systemctl enable lightdm
 
 echo "XFCE4 installed..."
